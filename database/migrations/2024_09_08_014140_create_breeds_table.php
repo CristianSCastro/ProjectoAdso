909bc11850d2154d7daa35d769b7aeca59b9f3c0
CristianSCastro <cscastro68@soy.sena.edu.co>
2024-09-07 23:18:04 -0400

    updated migrations

--- database/migrations/2024_09_08_014140_create_breeds_table.php
+++ database/migrations/2024_09_08_014140_create_breeds_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('breeds', function (Blueprint $table) {
             $table->id();
             $table->string('breedsName');
+            $table->string('test');
             $table->timestamps();
         });
     }
