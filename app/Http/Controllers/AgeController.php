3f61c4dccfe0603a2d5903cd9ce47d9b136ea12e
CristianSCastro <cscastro68@soy.sena.edu.co>
2024-09-14 12:21:44 -0400

    Fixed issued with routes to postman

--- app/Http/Controllers/AgeController.php
+++ app/Http/Controllers/AgeController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class AgeController extends Controller
 {
-    //
+    public function getData(Request $request)
+    { 
+        return response()->json([
+            'status' => '200',
+            'message' => 'Retrieved Successfully'
+        ]);
+    }
+
+          
+    public function save(request $request)
+        {
+             return response() ->json([
+            'status'=> '200',
+            'message' => 'Saved Successfully' 
+            ]);
+        }
+
+    public function update(request $request){
+
+        return response() ->json([
+            'status'=> '200',
+            'message' => 'Updated Successfully' 
+        ]);
+    }
+
+    
+    public function delete(request $request)
+    {
+
+        return response() ->json([
+            'status'=> '200',
+            'message' => 'Deleted Successfully' 
+        ]);
+    }
 }
